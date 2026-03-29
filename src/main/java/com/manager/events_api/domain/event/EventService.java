029b02b16a72e10f0a775381e2c4b7e6fc4e8dae
larissaolimpio <lrsolimpio@gmail.com>
2026-03-29 10:56:03 -0300

    feat: create EventService

---
+++ src/main/java/com/manager/events_api/domain/event/EventService.java
@@ -0,0 +1,20 @@
+package com.manager.events_api.domain.event;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    private final EventRepository repository;
+    private final EventMapper eventMapper;
+
+    public EventService(EventRepository repository, EventMapper eventMapper) {
+        this.repository = repository;
+        this.eventMapper = eventMapper;
+    }
+
+    public Event create(EventRequestDTO data) {
+        Event newEvent = eventMapper.map(data);
+        return repository.save(newEvent);
+    }
+}
