9e6aad7c12f02aad5464a5ee95b096216db10d45
larissaolimpio <lrsolimpio@gmail.com>
2026-04-01 09:06:12 -0300

    feat: Add method to get All Events and return a Page in controller

--- src/main/java/com/manager/events_api/domain/event/EventController.java
+++ src/main/java/com/manager/events_api/domain/event/EventController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+
 @RestController
 @RequestMapping("api/event")
 public class EventController {
