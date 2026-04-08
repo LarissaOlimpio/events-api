07e5f0eeb1d95a6c6af6c24ed518b272a9d0a94c
larissaolimpio <lrsolimpio@gmail.com>
2026-04-08 10:12:30 -0300

    refactor: remove access modifier public on interface EventRepository

--- src/main/java/com/manager/events_api/domain/event/EventRepository.java
+++ src/main/java/com/manager/events_api/domain/event/EventRepository.java
@@ -3,7 +3,6 @@ package com.manager.events_api.domain.event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.OffsetDateTime;
@@ -11,5 +10,5 @@ import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
-    public Page<Event> findByDateGreaterThanEqual(@Param("currentDate") OffsetDateTime currentDate, Pageable pageable);
+    Page<Event> findByDateGreaterThanEqual(@Param("currentDate") OffsetDateTime currentDate, Pageable pageable);
 }
