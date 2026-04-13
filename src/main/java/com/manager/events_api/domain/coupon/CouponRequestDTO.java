3a3566e2a2618cc6b6da665543876539800d3af0
larissaolimpio <lrsolimpio@gmail.com>
2026-04-13 15:23:36 -0300

    fix: remove import unnecessary

--- src/main/java/com/manager/events_api/domain/coupon/CouponRequestDTO.java
+++ src/main/java/com/manager/events_api/domain/coupon/CouponRequestDTO.java
@@ -1,7 +1,5 @@
 package com.manager.events_api.domain.coupon;
 
-import com.manager.events_api.domain.event.Event;
-
 import java.time.OffsetDateTime;
 
 public record CouponRequestDTO(String code, OffsetDateTime valid, Integer discount) {
