f81b8ef7d5c0ea1f6b69357cecb420b17f53bda6
larissaolimpio <lrsolimpio@gmail.com>
2026-04-13 15:45:22 -0300

    refactor: simplify mappers by removing redundant mappings

--- src/main/java/com/manager/events_api/domain/coupon/CouponMapper.java
+++ src/main/java/com/manager/events_api/domain/coupon/CouponMapper.java
@@ -1,14 +1,12 @@
 package com.manager.events_api.domain.coupon;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CouponMapper {
-    @Mapping(target = "id", ignore = true)
+
     Coupon map(CouponRequestDTO couponRequestDTO);
 
-    @Mapping(target = "eventId", source = "event.id")
     CouponResponseDTO toResponseDTO(Coupon coupon);
 }
--- src/main/java/com/manager/events_api/domain/event/EventMapper.java
+++ src/main/java/com/manager/events_api/domain/event/EventMapper.java
@@ -2,7 +2,6 @@ package com.manager.events_api.domain.event;
 
 import com.manager.events_api.domain.address.AddressMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(
@@ -10,9 +9,8 @@ import org.mapstruct.MappingConstants;
         uses = {AddressMapper.class}
 )
 public interface EventMapper {
-    @Mapping(target = "id", ignore = true)
+
     Event map(EventRequestDTO eventRequestDTO);
 
-    @Mapping(target = "id", source = "event.id")
     EventResponseDTO toResponseDTO(Event event);
 }
