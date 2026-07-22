1302e479928917c12ed5975d6a5f902fe155ce0f
larissaolimpio <lrsolimpio@gmail.com>
2026-07-22 20:50:28 -0300

    Add: unit Test for CouponService

--- src/main/java/com/manager/events_api/domain/coupon/CouponRequestDTO.java
+++ src/main/java/com/manager/events_api/domain/coupon/CouponRequestDTO.java
@@ -2,5 +2,10 @@ package com.manager.events_api.domain.coupon;
 
 import java.time.OffsetDateTime;
 
-public record CouponRequestDTO(String code, OffsetDateTime validFrom, OffsetDateTime validUntil, Integer discount) {
+public record CouponRequestDTO(
+        String code,
+        OffsetDateTime validFrom,
+        OffsetDateTime validUntil,
+        Integer discount
+) {
 }
--- src/test/java/com/manager/events_api/domain/coupon/CouponServiceTest.java
+++ src/test/java/com/manager/events_api/domain/coupon/CouponServiceTest.java
@@ -13,6 +13,7 @@ import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -25,14 +26,39 @@ public class CouponServiceTest {
     @Mock
     private CouponRepository couponRepository;
 
+    @Mock
+    private CouponMapper couponMapper;
+
     @InjectMocks
     private CouponService couponService;
 
     @Test
-    void shouldThrowExceptionWhenValidFromWasAfterValidUntil() {
+    void shouldAddCouponSuccessfullyWhenDatesAreValid() {
         Event event = new Event();
-        event.setDate(OffsetDateTime.now().plusDays(3));
+        UUID eventId = UUID.randomUUID();
 
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        CouponRequestDTO couponRequestDTO = new CouponRequestDTO(
+                "SUMMER10",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().plusDays(1),
+                20
+        );
+
+        Coupon mappedCoupon = new Coupon();
+        when(couponMapper.map(couponRequestDTO)).thenReturn(mappedCoupon);
+        when(couponRepository.save(any(Coupon.class))).thenReturn(mappedCoupon);
+        Coupon result = couponService.addCouponToEvent(eventId, couponRequestDTO);
+
+        assertNotNull(result);
+        verify(couponMapper, times(1)).map(couponRequestDTO);
+        verify(couponRepository, times(1)).save(any(Coupon.class));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValidFromWasAfterValidUntilInAddCouponToEvent() {
+        Event event = new Event();
         UUID eventId = UUID.randomUUID();
 
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
@@ -49,4 +75,82 @@ public class CouponServiceTest {
 
     }
 
+    @Test
+    void shouldThrowExceptionWhenValidUntilWasBeforeCurrentDateInAddCouponToEvent() {
+        Event event = new Event();
+        UUID eventId = UUID.randomUUID();
+
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+
+        CouponRequestDTO coupon = new CouponRequestDTO(
+                "SUMMER10",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().minusDays(3),
+                20
+        );
+
+        assertThrows(BusinessException.class, () -> couponService.addCouponToEvent(eventId, coupon));
+        verify(couponRepository, never()).save(any());
+    }
+
+    @Test
+    void shouldUpdateCouponSuccessfullyWhenDatesAreValid() {
+        Coupon existingCoupon = new Coupon();
+        UUID couponId = UUID.randomUUID();
+
+        when(couponRepository.findById(couponId)).thenReturn(Optional.of(existingCoupon));
+
+        CouponRequestDTO couponRequestDTO = new CouponRequestDTO(
+                "SUMMER10",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().plusDays(1),
+                20
+        );
+
+
+        when(couponRepository.save(any(Coupon.class))).thenReturn(existingCoupon);
+        couponService.updateCoupon(couponId, couponRequestDTO);
+
+        verify(couponMapper, times(1)).updateCouponFromDTO(couponRequestDTO, existingCoupon);
+        verify(couponRepository, times(1)).save(existingCoupon);
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValidFromWasAfterValidUntilInUpdateCoupon() {
+
+        Coupon coupon = new Coupon();
+        UUID couponId = UUID.randomUUID();
+
+        when(couponRepository.findById(couponId)).thenReturn(Optional.of(coupon));
+
+        CouponRequestDTO couponUpdate = new CouponRequestDTO(
+                "SUMMER10",
+                OffsetDateTime.now().plusDays(1),
+                OffsetDateTime.now(),
+                20
+        );
+
+        assertThrows(BusinessException.class, () -> couponService.updateCoupon(couponId, couponUpdate));
+        verify(couponRepository, never()).save(any());
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValidUntilWasBeforeCurrentDateInUpdateCoupon() {
+        Coupon coupon = new Coupon();
+        UUID couponId = UUID.randomUUID();
+
+        when(couponRepository.findById(couponId)).thenReturn(Optional.of(coupon));
+
+        CouponRequestDTO couponUpdate = new CouponRequestDTO(
+                "SUMMER10",
+                OffsetDateTime.now(),
+                OffsetDateTime.now().minusDays(3),
+                20
+        );
+
+        assertThrows(BusinessException.class, () -> couponService.updateCoupon(couponId, couponUpdate));
+        verify(couponRepository, never()).save(any());
+    }
 }
